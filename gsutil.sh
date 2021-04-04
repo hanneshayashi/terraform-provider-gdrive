fc46c61f087211529fda510e8c14e4b586d55fc5
Hannes Hayashi <hannes.siefert@gmail.com>
2021-04-04 09:55:28 +0200

    add CI config

---
+++ gsutil.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+APP_VERSION="0.2.0"
+APP_NAME="terraform-provider-gdrive"
+ARCHIVE_NAME=${APP_NAME}-${APP_VERSION}
+VERSIONS=("win-amd64" "linux-amd64" "linux-arm64" "mac-amd64" "mac-arm64")
+
+mkdir tmp
+for i in ${!VERSIONS[@]}; do
+    cp ./LICENSE ${APP_NAME}/${VERSIONS[$i]}/
+    tar -czf tmp/${ARCHIVE_NAME}_${VERSIONS[$i]}.tar.gz -C ${APP_NAME}/${VERSIONS[$i]} .
+done
+
+gsutil -m cp tmp/* gs://${BUCKET}/${APP_NAME}/
+
+for i in ${!VERSIONS[@]}; do
+    gsutil setmeta -h \"Content-Disposition:filename=${ARCHIVE_NAME}_${VERSIONS[$i]}.tar.gz\" gs://${BUCKET}/${APP_NAME}/${ARCHIVE_NAME}_${VERSIONS[$i]}.tar.gz
+done
