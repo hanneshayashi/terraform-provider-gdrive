93acca5eeb3bc3916c06a9d296cb6d25add84e4a
Hannes Hayashi <hannes.siefert@gmail.com>
2021-04-04 09:54:43 +0200

    update examlpes

--- example/main.tf
+++ example/main.tf
@@ -9,47 +9,48 @@ terraform {
 
 provider "gdrive" {
   service_account_key = "/path/to/sa.json"  # This is the Key file for your Service Account
+  # service_account     = "email@my-project.iam.gserviceaccount.com"  # This is the email address of your Service Account. You can leave this empty on GCE, if you want to use the instance's account
   subject             = "admin@example.com" # This is the user you want to impersonate with Domain Wide Delegation
 }
 
-resource "gdrive_drive" "test_drive" {
+resource "gdrive_drive" "example_drive" {
   name = "terraform-1"
 }
 
-resource "gdrive_permission" "test_permission" {
-  file_id                 = gdrive_drive.test_drive.id
-  email_address           = "user@example.com"
-  role                    = "reader"
-  type                    = "user"
-  send_notification_email = true
-  email_message           = "Test"
-}
-
-resource "gdrive_file" "folder-1" {
+resource "gdrive_file" "folder_1" {
   mime_type = "application/vnd.google-apps.folder"
-  drive_id  = gdrive_drive.test_drive.id
-  name      = "terraform-test"
-  parent    = gdrive_file.new-parent.id
+  drive_id  = gdrive_drive.example_drive.id
+  name      = "folder-1"
+  parent    = gdrive_drive.example_drive.id
 }
 
-resource "gdrive_file" "new-parent" {
+resource "gdrive_file" "folder_2" {
   mime_type = "application/vnd.google-apps.folder"
-  drive_id  = gdrive_drive.test_drive.id
-  name      = "root"
-  parent    = gdrive_drive.test_drive.id
-}
-
-resource "gdrive_permission" "test_permission-2" {
-  file_id       = gdrive_file.folder-1.id
-  email_address = "user2@example.com"
-  role          = "reader"
-  type          = "user"
+  drive_id  = gdrive_drive.example_drive.id
+  name      = "folder-2"
+  parent    = gdrive_file.folder_1.id
 }
 
 resource "gdrive_file" "content-1" {
   mime_type = "text/plain"
-  drive_id  = gdrive_drive.test_drive.id
+  drive_id  = gdrive_drive.example_drive.id
   name      = "somefile"
-  parent    = gdrive_drive.test_drive.id
+  parent    = gdrive_file.folder_2.id
   content   = "/path/to/somefile"
+}
+
+resource "gdrive_permission" "permissions_1" {
+  file_id                 = gdrive_drive.example_drive.id
+  email_address           = "user@example.com"
+  role                    = "reader"
+  type                    = "user"
+  send_notification_email = true
+  email_message           = "Example message"
+}
+
+resource "gdrive_permission" "permissions_2" {
+  file_id       = gdrive_file.folder_1.id
+  email_address = "user2@example.com"
+  role          = "reader"
+  type          = "user"
 }
