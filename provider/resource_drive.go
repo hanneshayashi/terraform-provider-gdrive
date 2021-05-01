21f571c907abc710be55155f6e65d1306761d5f7
Hannes Hayashi <26571811+hanneshayashi@users.noreply.github.com>
2021-05-01 13:40:58 +0200

    add drive restrictions

--- provider/resource_drive.go
+++ provider/resource_drive.go
@@ -37,6 +37,37 @@ func resourceDrive() *schema.Resource {
 				Optional:    true,
 				Description: "Use domain admin access",
 			},
+			"restrictions": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"admin_managed_restrictions": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "Whether administrative privileges on this shared drive are required to modify restrictions",
+						},
+						"copy_requires_writer_permission": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Description: `Whether the options to copy, print, or download files inside this shared drive, should be disabled for readers and commenters.
+When this restriction is set to true, it will override the similarly named field to true for any file inside this shared drive`,
+						},
+						"domain_users_only": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Description: `Whether access to this shared drive and items inside this shared drive is restricted to users of the domain to which this shared drive belongs.
+This restriction may be overridden by other sharing policies controlled outside of this shared drive`,
+						},
+						"drive_members_only": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "Whether access to items inside this shared drive is restricted to its members",
+						},
+					},
+				},
+			},
 		},
 		Create: resourceCreateDrive,
 		Read:   resourceReadDrive,
@@ -53,30 +84,138 @@ func resourceDrive() *schema.Resource {
 // 	return nil, nil
 // }
 
+func dataToDrive(d *schema.ResourceData, update bool) (*drive.Drive, error) {
+	// var err error
+	newDrive := &drive.Drive{}
+	// if d.HasChange("backgroundImageFile") {
+	// 	backgroundImageFile := d.Get("backgroundImageFile").(string)
+	// 	if backgroundImageFile != "" {
+	// 		m := gsmhelpers.FlagToMap(backgroundImageFile)
+	// 		newDrive.BackgroundImageFile = &drive.DriveBackgroundImageFile{
+	// 			Id: m["id"],
+	// 		}
+	// 		newDrive.BackgroundImageFile.Width, err = strconv.ParseFloat(m["width"], 64)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+	// 		newDrive.BackgroundImageFile.XCoordinate, err = strconv.ParseFloat(m["xCoordinate"], 64)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+	// 		newDrive.BackgroundImageFile.YCoordinate, err = strconv.ParseFloat(m["yCoordinate"], 64)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+	// 	} else {
+	// 		newDrive.ForceSendFields = append(newDrive.ForceSendFields, "BackgroundImageFile")
+	// 	}
+	// }
+	// if d.Get("themeId").IsSet() {
+	// 	newDrive.ThemeId = d.Get("themeId").(string)
+	// 	if newDrive.ThemeId == "" {
+	// 		newDrive.ForceSendFields = append(newDrive.ForceSendFields, "ThemeId")
+	// 	}
+	// }
+	// if d.Get("colorRgb").IsSet() {
+	// 	newDrive.ColorRgb = d.Get("colorRgb").(string)
+	// 	if newDrive.ColorRgb == "" {
+	// 		newDrive.ForceSendFields = append(newDrive.ForceSendFields, "ColorRgb")
+	// 	}
+	// }
+	if d.HasChange("name") {
+		newDrive.Name = d.Get("name").(string)
+		if newDrive.Name == "" {
+			newDrive.ForceSendFields = append(newDrive.ForceSendFields, "Name")
+		}
+	}
+	if update {
+		if d.HasChange("restrictions") {
+			newDrive.Restrictions = &drive.DriveRestrictions{}
+			restrictions := d.Get("restrictions").([]interface{})
+			if len(restrictions) > 0 {
+				if d.HasChange("restrictions.0.admin_managed_restrictions") {
+					newDrive.Restrictions.AdminManagedRestrictions = d.Get("restrictions.0.admin_managed_restrictions").(bool)
+					if !newDrive.Restrictions.AdminManagedRestrictions {
+						newDrive.Restrictions.ForceSendFields = append(newDrive.Restrictions.ForceSendFields, "AdminManagedRestrictions")
+					}
+				}
+				if d.HasChange("restrictions.0.copy_requires_writer_permission") {
+					newDrive.Restrictions.CopyRequiresWriterPermission = d.Get("restrictions.0.copy_requires_writer_permission").(bool)
+					if !newDrive.Restrictions.CopyRequiresWriterPermission {
+						newDrive.Restrictions.ForceSendFields = append(newDrive.Restrictions.ForceSendFields, "CopyRequiresWriterPermission")
+					}
+				}
+				if d.HasChange("restrictions.0.domain_users_only") {
+					newDrive.Restrictions.DomainUsersOnly = d.Get("restrictions.0.domain_users_only").(bool)
+					if !newDrive.Restrictions.DomainUsersOnly {
+						newDrive.Restrictions.ForceSendFields = append(newDrive.Restrictions.ForceSendFields, "DomainUsersOnly")
+					}
+				}
+				if d.HasChange("restrictions.0.drive_members_only") {
+					newDrive.Restrictions.DriveMembersOnly = d.Get("restrictions.0.drive_members_only").(bool)
+					if !newDrive.Restrictions.DriveMembersOnly {
+						newDrive.Restrictions.ForceSendFields = append(newDrive.Restrictions.ForceSendFields, "DriveMembersOnly")
+					}
+				}
+			} else {
+				newDrive.Restrictions.ForceSendFields = append(newDrive.Restrictions.ForceSendFields, "AdminManagedRestrictions", "CopyRequiresWriterPermission", "DomainUsersOnly", "DriveMembersOnly")
+			}
+		}
+	}
+	return newDrive, nil
+}
+
 func resourceCreateDrive(d *schema.ResourceData, _ interface{}) error {
-	r, err := gsmdrive.CreateDrive(&drive.Drive{Name: d.Get("name").(string)}, "", true)
+	var driveResult *drive.Drive
+	var err error
+	driveRequest, err := dataToDrive(d, false)
 	if err != nil {
 		return err
 	}
-	d.SetId(r.Id)
-	err = resourceReadDrive(d, nil)
+	driveResult, err = gsmdrive.CreateDrive(driveRequest, "*", true)
 	if err != nil {
 		return err
+	}
+	d.SetId(driveResult.Id)
+	if d.HasChange("restrictions") {
+		err = resourceUpdateDrive(d, nil)
+		if err != nil {
+			return err
+		}
+	} else {
+		err = resourceReadDrive(d, nil)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
 func resourceReadDrive(d *schema.ResourceData, _ interface{}) error {
-	r, err := gsmdrive.GetDrive(d.Id(), "name", d.Get("use_domain_admin_access").(bool))
+	r, err := gsmdrive.GetDrive(d.Id(), "*", d.Get("use_domain_admin_access").(bool))
 	if err != nil {
 		return err
 	}
 	d.Set("name", r.Name)
+	restrictions := make([]map[string]bool, 1)
+	if r.Restrictions != nil {
+		restrictions[0] = map[string]bool{
+			"admin_managed_restrictions":      r.Restrictions.AdminManagedRestrictions,
+			"copy_requires_writer_permission": r.Restrictions.CopyRequiresWriterPermission,
+			"domain_users_only":               r.Restrictions.DomainUsersOnly,
+			"drive_members_only":              r.Restrictions.DriveMembersOnly,
+		}
+	}
+	d.Set("restrictions", restrictions)
 	return nil
 }
 
 func resourceUpdateDrive(d *schema.ResourceData, _ interface{}) error {
-	_, err := gsmdrive.UpdateDrive(d.Id(), "id", d.Get("use_domain_admin_access").(bool), &drive.Drive{Name: d.Get("name").(string)})
+	driveRequest, err := dataToDrive(d, true)
+	if err != nil {
+		return err
+	}
+	_, err = gsmdrive.UpdateDrive(d.Id(), "id", d.Get("use_domain_admin_access").(bool), driveRequest)
 	if err != nil {
 		return err
 	}
