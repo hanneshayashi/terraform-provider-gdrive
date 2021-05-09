f5eca0156d816ea3587a1bfddeef4c15ef194832
Hannes Hayashi <26571811+hanneshayashi@users.noreply.github.com>
2021-05-09 09:21:16 +0200

    V0.4.0 (#4)
    * fix package comments
    
    * add download and export to file data source
    
    * update file docs
    
    * update version in example
    
    * update deps
    
    * delete old build scripts
    
    * update readme

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     gdrive = {
       source  = "hanneshayashi/gdrive"
-      version = "0.3.0"
+      version = "0.4.0"
     }
   }
 }
--- provider/helpers.go
+++ provider/helpers.go
@@ -1,5 +1,4 @@
 /*
-Package provider implements the Terraform provider
 Copyright © 2021 Hannes Hayashi
 
 This program is free software: you can redistribute it and/or modify
@@ -15,6 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+
 package provider
 
 func contains(s string, slice []string) bool {
