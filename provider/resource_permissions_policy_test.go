ce1903cca667ce39b90abaa2a12153bfe6b04a9d
Hannes Hayashi <26571811+hanneshayashi@users.noreply.github.com>
2023-06-22 09:55:50 +0200

    rename file

--- provider/resource_permissions_policy_test.go
+++ provider/resource_permissions_policy_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccPermissionPolicy(t *testing.T) {
+func TestAccPermissionsPolicy(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
